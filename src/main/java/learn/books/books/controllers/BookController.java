9226c808dae47f24d6111c63ba50deef983f763d
Dikshant23529 <latherdikshant0@gmail.com>
2023-12-24 22:20:50 +0530

    post request is also working fine

--- src/main/java/learn/books/books/controllers/BookController.java
+++ src/main/java/learn/books/books/controllers/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import learn.books.books.entities.Book;
@@ -31,8 +32,11 @@ public class BookController {
     }
 
     @PostMapping("/books")
-    public ResponseEntity<Book> createBook(Book book) {
-                return ResponseEntity.ok(book);
+    public Book createBook(@RequestBody Book book) {
+
+        Book b = this.bookService.postBook(book);
+        return b;
+
     }
 
 }
